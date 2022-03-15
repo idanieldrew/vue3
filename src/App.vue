9f3451a3573aa553937771a76614b0c534cd3be9
idanieldrew <danielgia35@gmail.com>
2022-03-15 23:25:07 +0330

    Some work

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,10 @@
 <template>
   <div>
-    <table
-      class="
-        border-separate border border-slate-500
-        container
-        mx-auto
-        table-auto
-      "
-    >
-      <thead>
-        <tr class="bg-black">
-          <th class="border border-slate-600 text-white py-5">Name</th>
-          <th class="border border-slate-600 text-white py-5">Email</th>
-          <th class="border border-slate-600 text-white py-5">Update</th>
-          <th class="border border-slate-600 text-white py-5">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in allUsers" :key="user.id" class="bg-gray-700">
-          <td class="border border-slate-700 text-white py-3">
-            {{ user.name }}
-          </td>
-          <td class="border border-slate-700 text-white">{{ user.email }}</td>
-          <td class="border border-slate-700 text-white">Update</td>
-          <td class="border border-slate-700 text-white">Delete</td>
-        </tr>
-      </tbody>
-    </table>
+
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <!-- <router-view/> -->
+  <router-view/> 
 </template>
 
 <script lang="ts">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <table
+      class="
+        border-separate border border-slate-500
+        container
+        mx-auto
+        table-auto
+      "
+    >
+      <thead>
+        <tr class="bg-gray-300">
+          <th class="border-2 border-red-500 text-black py-5">Name</th>
+          <th class="border-red-500 border-2 text-black py-5">Email</th>
+          <th class="border-red-500 border-2 text-black py-5">Update</th>
+          <th class="border-red-500 border-2 text-black py-5">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in allUsers" :key="user.id" class="bg-gray-700">
+          <td class="border border-slate-700 text-white py-3">
+            {{ user.name }}
+          </td>
+          <td class="border border-slate-700 text-white">{{ user.email }}</td>
+          <td class="border border-slate-700 text-white">
+            <router-link
+              class="hover:text-green-400"
+              :to="'/update/' + user.name"
+              >Update</router-link
+            >
+          </td>
+          <td class="border border-slate-700 text-white">
+            <router-link class="hover:text-red-400" to="/">Delete</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { computed } from "vue-demi";
+import { auth } from "./../store";
+export default {
+  setup() {
+    const store = auth();
 
-@Options({
-  components: {
-    HelloWorld,
+    store.fetchPosts();
+
+    const allUsers = computed(() => store.allUsers);
+    return { allUsers };
   },
-})
-export default class Home extends Vue {}
+};
 </script>
